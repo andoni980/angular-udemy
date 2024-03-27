f8ece6f2039b40f314494b998dd5d91a5d4e60ff
andoni980 <andonilarrazabal980@gmail.com>
2024-03-27 13:26:50 +0100

    02-objects-interface

---
+++ 01-typescript-intro/src/topics/02-object-interface.ts
@@ -0,0 +1,26 @@
+
+const skills: string[] = ['Bash', 'Counter', 'Healing'];
+
+
+interface Character {
+    name: string;
+    hp: number;
+    skills: string[];
+    homeTown?: string | undefined;
+}
+
+const strider: Character = {
+
+    name: 'Strider',
+    hp: 100,
+    skills: ['Bash', 'Counter'],
+}
+
+strider.homeTown = 'Rivendell';
+
+console.table(strider);
+
+
+
+
+export {};
