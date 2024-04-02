b1c08a29f7f4fda27affcdfa708cf1b31fe0708c
andoni980 <andonilarrazabal980@gmail.com>
2024-04-02 21:26:45 +0200

    Decoradores

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -8,8 +8,8 @@ import './style.css'
 // import './topics/06-function-destructuring.ts'
 // import './topics/07-import-export.ts'
 // import './topics/08-classes.ts'
-import './topics/09-generics.ts'
-
+// import './topics/09-generics.ts'
+import './topics/10-decorators'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 Hola mundo
