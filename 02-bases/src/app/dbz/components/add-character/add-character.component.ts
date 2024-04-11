0e9548b6720a7c7f0c5a5317fd767975830b4632
andoni980 <andonilarrazabal980@gmail.com>
2024-04-11 19:49:55 +0200

    ngModel en addCharacter

--- 02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css']
 })
-export class AddCharacterComponent implements OnInit {
+export class AddCharacterComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  public character: Character = {
+    name: '',
+    power: 0
+  };
+
+  emitCharacter(): void {
+    console.log(this.character);
   }
-
 }
